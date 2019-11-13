bf688afc7ceb84496716d1c8f6ffe8d7f1955167
Richard <wei.richard.s@gmail.com>
2019-11-13 03:48:33 -0800

    complete move find algorithm

--- src/Chess.js
+++ src/Chess.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './chess.css';
-const chessHelpers = require('./chessHelpers.js');
+import * as chessHelpers from './chessHelpers.js'
 
 class Chess extends Component {
 	constructor(props) {
@@ -22,6 +22,7 @@ class Chess extends Component {
 	}
 
 	render() {
+		console.log(this.state.pieces)
 		return this.state.pieces && <Board className="board"
 			pieces = {this.state.pieces}
 		></Board>
@@ -43,14 +44,9 @@ class Board extends Component {
 			pieceID={null}
 			rank={rank}
 			file={file}
-			piece={piece}
+			chesspiece={piece}
+			boardSetup={this.props.pieces}
 		></Square>
-	}
-	
-	findPieceAt(rank, file, piecesObject) {
-		const rankPieces = piecesObject[rank]
-		const filePieces = rankPieces ? piecesObject[rank][file] : null
-		return filePieces
 	}
 
 	render() {
@@ -66,7 +62,7 @@ class Board extends Component {
 								const rank = row;
 								const file = column;
 								const squareId = row*8+file;
-								const piece=this.findPieceAt(rank, file, pieces);
+								const piece=chessHelpers.findPieceAt(rank, file, pieces);
 								return this.renderSquare(squareId, rank, file, piece)
 							})
 						}
@@ -79,33 +75,98 @@ class Board extends Component {
 }
 
 class Square extends Component {
-	// renderPiece(piece) {
+	// renderPiece(chesspiece) {
 	// 	return <Piece></Piece>
 	// }
 
 	render() {
+		const currentChesspiece = this.props.chesspiece;
+		const pieceColor = currentChesspiece ? currentChesspiece[0] : null;
+		const pieceType = currentChesspiece ? currentChesspiece[1] : null;
 		return <button className={`square square-${this.props.color}`}>
-			<Piece
-				chesspiece={this.props.piece}
-			></Piece>
+			{currentChesspiece && <Piece
+				symbol={chessHelpers.chess_unicode[pieceColor][pieceType]}
+				color={pieceColor}
+				boardSetup={this.props.boardSetup}
+				coordinate={[this.props.file,this.props.rank]}
+				{...chessHelpers.pieceAttributes[pieceType]}
+			></Piece>}
 		</button>
 	}
 
 }
 
 class Piece extends Component {
+	getEligibleMoves(piece) {
+		const currentPosition = piece.coordinate;
+		const unitMoves = piece.unitMoves;
+		// first check for any directions that don't immediately go off-board
+		const newCoordinates = unitMoves.map((deltaCoord) => {
+			const negativeIfWhite = piece.color==='white' ? -1 : 1;  
+			return [(currentPosition[0]+negativeIfWhite*deltaCoord[0]), (currentPosition[1]+negativeIfWhite*deltaCoord[1])] 
+		});
+		const eligibleUnitMoves = newCoordinates.filter((coord) => {
+			// eliminate off-board coords
+			if (coord[0]<0 || coord[0]>7 || coord[1]<0 || coord[1]>7) {return false}
+			// see if occupant exists in space 
+			const occupant = chessHelpers.findPieceAt(coord[1], coord[0], piece.boardSetup);
+			const occupantColor = occupant ? occupant[0] : null;
+			return (occupantColor===piece.color) ? false : true;
+		})
+		if (!piece.unlimitedRange) {
+		// if piece does not have unlimited range
+			return eligibleUnitMoves;
+		}	else {
+			if (eligibleUnitMoves.length===0) return null;
+			// take the eligible unit moves
+			const eligibleDeltas = eligibleUnitMoves.map((coord)=>{
+				return [coord[0]-currentPosition[0], coord[1]-currentPosition[1]]
+			});
+			const movesUnlimitedRange = eligibleDeltas.map((deltaCoord)=>{
+				const allMovesForDelta = this.extendForUnlimitedRange(piece, currentPosition, deltaCoord)
+				console.log(allMovesForDelta)
+				return allMovesForDelta
+			});
+		}
+	}
+
+	extendForUnlimitedRange(piece, currentPosition, deltaMovement, eligibleMoves=[]) {
+		const dx=deltaMovement[0];
+		const dy=deltaMovement[1];
+		const newCoord = [currentPosition[0]+dx, currentPosition[1]+dy]
+		// check if valid
+		// eliminate off-board coords
+		if (newCoord[0]<0 || newCoord[0]>7 || newCoord[1]<0 || newCoord[1]>7) {return eligibleMoves}
+		// see if occupant exists in space 
+		const occupant = chessHelpers.findPieceAt(newCoord[1], newCoord[0], piece.boardSetup);
+		const occupantColor = occupant ? occupant[0] : null;
+		if (occupantColor===piece.color) {
+			return eligibleMoves;
+		} else if (occupantColor && occupantColor !==piece.color) {
+			eligibleMoves.push(newCoord)
+			return eligibleMoves;
+		}
+		// if we get to this point we have an eligible coord
+		// push and recurse
+		eligibleMoves.push(newCoord);
+		return this.extendForUnlimitedRange(piece, newCoord, deltaMovement, eligibleMoves)
+	}
+
 	render() {
+		if (this.props.type==='queen') {
+			let moves = this.getEligibleMoves(this.props)
+
+		}
 		return <div>
-			{chessHelpers.chess_unicode[this.props.chesspiece]}
+			{this.props.symbol}
 		</div>
 	}
 }
 
-
-
-
 class Player extends Component {
 
 }
+
+
 
 export default Chess;
--- src/chessHelpers.js
+++ src/chessHelpers.js
@@ -1,86 +1,159 @@
-const defaultSetupWhite = {
-	// Hash by ranks white POV
-	// Nest by file
-	0: { 0:'black-rook',1:'black-knight',2:'black-bishop',3:'black-queen',4:'black-king',5:'black-bishop',6:'black-knight',7:'black-rook' },
-	1: { 0:'black-pawn',1:'black-pawn',2:'black-pawn',3:'black-pawn',4:'black-pawn',5:'black-pawn',6:'black-pawn',7:'black-pawn' },
-	6: { 0:'white-pawn',1:'white-pawn',2:'white-pawn',3:'white-pawn',4:'white-pawn',5:'white-pawn',6:'white-pawn',7:'white-pawn' },
-	7: { 0:'white-rook',1:'white-knight',2:'white-bishop',3:'white-queen',4:'white-king',5:'white-bishop',6:'white-knight',7:'white-rook' },
-}
-const defaultSetupBlack = {
-	// Hash by ranks black POV
-	0: { 0:'white-rook',1:'white-knight',2:'white-bishop',3:'white-king',4:'white-queen',5:'white-bishop',6:'white-knight',7:'white-rook' },
-	1: { 0:'white-pawn',1:'white-pawn',2:'white-pawn',3:'white-pawn',4:'white-pawn',5:'white-pawn',6:'white-pawn',7:'white-pawn' },
-	6: { 0:'black-pawn',1:'black-pawn',2:'black-pawn',3:'black-pawn',4:'black-pawn',5:'black-pawn',6:'black-pawn',7:'black-pawn' },
-	7: { 0:'black-rook',1:'black-knight',2:'black-bishop',3:'black-king',4:'black-queen',5:'black-bishop',6:'black-knight',7:'black-rook' },
-}
 
-const chess_unicode = {
-	'white-king': '\u2654',
-	'white-queen': '\u2655',
-	'white-rook': '\u2656',
-	'white-bishop': '\u2657',
-	'white-knight': '\u2658',
-	'white-pawn': '\u2659',
-	'black-king': '\u265A',
-	'black-queen': '\u265B',
-	'black-rook': '\u265C',
-	'black-bishop': '\u265D',
-	'black-knight': '\u265E',
-	'black-pawn': '\u265F',
+
+export function findPieceAt(rank, file, piecesObject) {
+	try {
+		return piecesObject[rank][file]
+	} 
+	catch(error) {
+		// console.log(`No piece at (${rank},${file})`)
+		return null;
+	}
 }
 
 
-const pieceAttributesByType = {
+export const chess_unicode = {
+	white: {king: '\u2654',
+		queen: '\u2655',
+		rook: '\u2656',
+		bishop: '\u2657',
+		knight: '\u2658',
+		pawn: '\u2659'
+	},
+	black: {king: '\u265A',
+		queen: '\u265B',
+		rook: '\u265C',
+		bishop: '\u265D',
+		knight: '\u265E',
+		pawn: '\u265F'
+	}
+}
+
+export const pieceAttributesByType = {
 	// moves in form of [dx, dy]
 	king: {
+		type: 'king',
 		unlimitedRange: false,
-		moveset: [[0,1],[0,-1],[-1,0],[1,0],[1,1],[-1,-1],[-1,1],[1,-1]],
+		unitMoves: [[0,1],[0,-1],[-1,0],[1,0],[1,1],[-1,-1],[-1,1],[1,-1]],
 		hasMoved: false,
 		inCheck: false
 	},
 	queen: {
+		type: 'queen',
 		unlimitedRange: true,
-		moveset: [[0,1],[0,-1],[-1,0],[1,0],[1,1],[-1,-1],[-1,1],[1,-1]],
+		unitMoves: [[0,1],[0,-1],[-1,0],[1,0],[1,1],[-1,-1],[-1,1],[1,-1]],
 	},
 	rook: {
+		type: 'rook',
 		unlimitedRange: true,
-		moveset: [[0,1],[0,-1],[-1,0],[1,0]],
+		unitMoves: [[0,1],[0,-1],[-1,0],[1,0]],
 		hasMoved: false
 	},
 	bishop: {
+		type: 'bishop',
 		unlimitedRange: true,
-		moveset: [[1,1],[-1,-1],[-1,1],[1,-1]],
+		unitMoves: [[1,1],[-1,-1],[-1,1],[1,-1]],
 	},
 	knight: {
+		type: 'knight',
 		unlimitedRange: false,
-		moveset: [[3,2],[-3,-2],[3,-2],[-3,2],[2,3],[-2,-3],[2,-3],[-2,3]],
+		unitMoves: [[1,2],[-1,-2],[1,-2],[-1,2],[2,1],[-2,-1],[2,-1],[-2,1]],
 	},
 	pawn: {
+		type: 'pawn',
 		unlimitedRange: false,
-		moveset: [0,1],
+		unitMoves: [[0,1]],
 		captureMoveset: [], 
 		hasMoved: false
 	}
 }
 
-const pieceAttributes = {
-	'white-king': pieceAttributesByType.king,
-	'white-queen': pieceAttributesByType.queen,
-	'white-rook': pieceAttributesByType.rook,
-	'white-bishop': pieceAttributesByType.bishop,
-	'white-knight': pieceAttributesByType.knight,
-	'white-pawn': pieceAttributesByType.pawn,
-	'black-king': pieceAttributesByType.king,
-	'black-queen': pieceAttributesByType.queen,
-	'black-rook': pieceAttributesByType.rook,
-	'black-bishop': pieceAttributesByType.bishop,
-	'black-knight': pieceAttributesByType.knight,
-	'black-pawn': pieceAttributesByType.pawn,
+export const pieceAttributes = {
+	'king': pieceAttributesByType.king,
+	'queen': pieceAttributesByType.queen,
+	'rook': pieceAttributesByType.rook,
+	'bishop': pieceAttributesByType.bishop,
+	'knight': pieceAttributesByType.knight,
+	'pawn': pieceAttributesByType.pawn,
 }
 
-module.exports = {
-	defaultSetupWhite: defaultSetupWhite,
-	defaultSetupBlack: defaultSetupBlack,
-	chess_unicode: chess_unicode,
-	pieceAttributes: pieceAttributes,
+export const defaultSetupWhite = {
+	// Hash by ranks white POV
+	// Nest by file
+	0: { 
+		0:['black','rook'],
+		1:['black','knight'],
+		2:['black','bishop'],
+		3:['black','queen'],
+		4:['black','king'],
+		5:['black','bishop'],
+		6:['black','knight'],
+		7:['black','rook'] 
+	}, 1: { 
+		0:['black','pawn'],
+		1:['black','pawn'],
+		2:['black','pawn'],
+		3:['black','pawn'],
+		4:['black','pawn'],
+		5:['black','pawn'],
+		6:['black','pawn'],
+		7:['black','pawn'] 
+	}, 6: { 
+		0:['white','pawn'],
+		1:['white','pawn'],
+		2:['white','pawn'],
+		/*3:['white','pawn'],*/
+		4:['white','pawn'],
+		5:['white','pawn'],
+		6:['white','pawn'],
+		7:['white','pawn'] 
+	}, 7: { 
+		0:['white','rook'],
+		1:['white','knight'],
+		2:['white','bishop'],
+		3:['white','queen'],
+		4:['white','king'],
+		5:['white','bishop'],
+		6:['white','knight'],
+		7:['white','rook'] 
+	}
+}
+export const defaultSetupBlack = {
+	// Hash by ranks black POV
+	0: { 
+		0:['white','rook'],
+		1:['white','knight'],
+		2:['white','bishop'],
+		3:['white','king'],
+		4:['white','queen'],
+		5:['white','bishop'],
+		6:['white','knight'],
+		7:['white','rook'] 
+	}, 1: { 
+		0:['white','pawn'],
+		1:['white','pawn'],
+		2:['white','pawn'],
+		3:['white','pawn'],
+		4:['white','pawn'],
+		5:['white','pawn'],
+		6:['white','pawn'],
+		7:['white','pawn'] 
+	}, 6: { 
+		0:['black','pawn'],
+		1:['black','pawn'],
+		2:['black','pawn'],
+		3:['black','pawn'],
+		4:['black','pawn'],
+		5:['black','pawn'],
+		6:['black','pawn'],
+		7:['black','pawn'] 
+	}, 7: { 
+		0:['black','rook'],
+		1:['black','knight'],
+		2:['black','bishop'],
+		3:['black','king'],
+		4:['black','queen'],
+		5:['black','bishop'],
+		6:['black','knight'],
+		7:['black','rook'] 
+	}
 }
