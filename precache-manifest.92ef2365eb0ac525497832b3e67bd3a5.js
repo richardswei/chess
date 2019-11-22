c71cddef273426b660321353c2885989943854b5
Richard <wei.richard.s@gmail.com>
2019-11-21 20:11:25 -0800

    Updates

---
+++ precache-manifest.92ef2365eb0ac525497832b3e67bd3a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663c00a762c87c8cc82574e967a78abd",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "ef37aa179daec039ca9a",
+    "url": "/chess/static/css/main.55c48fe8.chunk.css"
+  },
+  {
+    "revision": "55b0ceb059709d6c7549",
+    "url": "/chess/static/js/2.089cd836.chunk.js"
+  },
+  {
+    "revision": "ef37aa179daec039ca9a",
+    "url": "/chess/static/js/main.2f7b45e6.chunk.js"
+  },
+  {
+    "revision": "8b906bb5f78d6c60e03f",
+    "url": "/chess/static/js/runtime-main.e352592b.js"
+  }
+]);
